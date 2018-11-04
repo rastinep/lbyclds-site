75fb893d413a97531ddc2b6c9f2eb3ab00a1a694
Rastine Pinlac <rastine_pinlac@dlsu.edu.ph>
2018-11-04 14:49:09 +0800

    Add files via upload

---
+++ cnisproject/cnisblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CnisblogConfig(AppConfig):
+    name = 'cnisblog'
---
+++ cnisproject/cnisblog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'author': 'Rastine Pinlac',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Shenn Tinsay',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 28, 2018'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'cnisblog/home.html', context)
+
+
+def about(request):
+    return render(request, 'cnisblog/about.html', {'title': 'About'})
